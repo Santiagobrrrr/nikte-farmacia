e09d9340c90164dc58de8831ee10dfb396e18c41
Santiagobrrrr <sjbatz@correo.url.edu.gt>
2026-05-06 09:52:20 -0600

    correccion ya para unir

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', '127.0.0.1');
 define('DB_PORT', '3306');
-define('DB_NAME', 'farmacia_nikte');
+define('DB_NAME', 'nikte_farmacia');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
